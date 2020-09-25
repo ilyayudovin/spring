7b38ee7f91ddc0a7307989930c9246e02a5238a6
ilyayudovin <ilyayudovin123@gmail.com>
2020-09-25 12:28:15 +0300

    Delete excess spaces

--- src/CardsList/CardsList.js
+++ src/CardsList/CardsList.js
@@ -5,14 +5,13 @@ import '../Styles/CardList.scss';
 const CardsList = ({ items }) => (
   <div className="cardListContainer">
     {
-        items.length
-          ? (
-            <div className="cardList">
-              {items.map((item) => <Card name={item.name} text={item.text} icon={item.icon} />)}
-            </div>
-          )
-          : <div className="noResults">No Results</div>
-      }
+      items.length
+        ? (
+          <div className="cardList">
+            {items.map((item) => <Card name={item.name} text={item.text} icon={item.icon} />)}</div>
+        )
+        : <div className="noResults">No Results</div>
+    }
   </div>
 );
 
