a5634d7c3bff6861a591967fd402d73c45329e38
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-09 16:04:26 +0300

    Add basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './styles/App.scss';
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Redirect to='/login'/>
-      </Route>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/home" component={Home}/>
-    </Switch>
-  </BrowserRouter>
-);
+import {BrowserRouter, Switch, Route, useHistory} from 'react-router-dom';
+const App = () => {
+  const history = useHistory();
+  console.log(history);
+  return (
+    <BrowserRouter basename="/spring">
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/home" component={Home}/>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(<App/>, document.getElementById('root'));
