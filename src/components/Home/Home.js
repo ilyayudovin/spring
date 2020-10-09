a6c32e136cedb78430b6b54fc0878cf598f5fa87
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-09 17:18:20 +0300

    Fix eslint errors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import Navigation from "../Navigation/Navigation";
-import Introduction from "../Introduction/Introduction";
-import CardsList from "../CardsList/CardsList";
-import AnchorsSection from "../AnchorsSection/AnchorsSection";
-import Footer from "../Footer/Footer";
+import Navigation from '../Navigation/Navigation';
+import Introduction from '../Introduction/Introduction';
+import CardsList from '../CardsList/CardsList';
+import AnchorsSection from '../AnchorsSection/AnchorsSection';
+import Footer from '../Footer/Footer';
 
-const Home = () => {
-  return (
-    <>
-      <Navigation />
-      <Introduction />
-      <CardsList />
-      <AnchorsSection />
-      <Footer />
-    </>
-  )
-};
+const Home = () => (
+  <>
+    <Navigation />
+    <Introduction />
+    <CardsList />
+    <AnchorsSection />
+    <Footer />
+  </>
+);
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/App.scss';
-import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import {BrowserRouter, Switch, Route, useHistory} from 'react-router-dom';
-const App = () => {
-  const history = useHistory();
-  console.log(history);
-  return (
-    <BrowserRouter basename="/spring">
-      <Switch>
-        <Route exact path="/" component={Login}/>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/home" component={Home}/>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const App = () => (
+  <BrowserRouter basename="/spring">
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/home" component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
