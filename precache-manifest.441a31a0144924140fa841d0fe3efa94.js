e40acb94f02543171c61554dd46870e4a36826f5
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-09 12:02:12 +0300

    Updates

---
+++ precache-manifest.441a31a0144924140fa841d0fe3efa94.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f571ac4fea4cab289122ec7a92b88",
+    "url": "/spring/index.html"
+  },
+  {
+    "revision": "39c084f11c90a4b7e1ce",
+    "url": "/spring/static/css/main.78a7603e.chunk.css"
+  },
+  {
+    "revision": "16589e760cfd875edbd6",
+    "url": "/spring/static/js/2.2003f40b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/spring/static/js/2.2003f40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c084f11c90a4b7e1ce",
+    "url": "/spring/static/js/main.62a4a8a9.chunk.js"
+  },
+  {
+    "revision": "617c3d37ad0566d4cb4a",
+    "url": "/spring/static/js/runtime-main.e7083fa7.js"
+  },
+  {
+    "revision": "e46d1ff3bbd7865b7bdfbd2bc74fafdf",
+    "url": "/spring/static/media/Metropolis-Bold.e46d1ff3.ttf"
+  },
+  {
+    "revision": "8e113ffbceb397a8af63536cc74969c6",
+    "url": "/spring/static/media/Metropolis-Regular.8e113ffb.ttf"
+  },
+  {
+    "revision": "364db56cc72012f4f54d70e13ac0a0d9",
+    "url": "/spring/static/media/OG-Spring.svg.364db56c.png"
+  },
+  {
+    "revision": "438c93c081b9186539c398ef078a37c6",
+    "url": "/spring/static/media/github.438c93c0.png"
+  },
+  {
+    "revision": "d35864ebbc212e0bde90c7e56c3c22eb",
+    "url": "/spring/static/media/springrestdocs.d35864eb.png"
+  }
+]);
