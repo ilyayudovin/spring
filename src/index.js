10de20daac48826397a9a05b6ed0d1f592b01181
ilyayudovin <ilyayudovin123@gmail.com>
2020-10-23 18:22:00 +0300

    Create signup route

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './styles/App.scss';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 import RequireAuth from './components/RequireAuth/RequireAuth';
 
 const App = () => (
   <BrowserRouter basename="/spring">
     <Switch>
       <Route exact path="/" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
       <Route exact path="/login" component={Login} />
       <RequireAuth>
         <Route exact path="/home" component={Home} />
